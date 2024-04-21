8fcd22cfcda9fdc1c17f85c0f382ca6906e96635
realandyy <145111682+realandyy@users.noreply.github.com>
2024-04-21 20:02:58 +0200

    Order.java

--- JavaTerm2Project/Order.java
+++ JavaTerm2Project/Order.java
@@ -1,5 +1,3 @@
-
-
 package JavaTerm2Project;
 
 import javax.swing.*;
@@ -9,7 +7,6 @@ public class Order {
     private int nProducts;
     private Product[] prod = new Product[nProducts];
     private float totalPrice;
-
     public Order(int customerId, Product[] prod) {
         this.customerId = customerId;
         this.prod = prod;
@@ -37,21 +34,16 @@ public class Order {
         String customerID = "Customer ID: " + cart.getCustomerId() + '\n';
         String products = "Products: \n";
         String totalPrice = "Total Price: $" + cart.calculatePrice() + '\n';           // Order details
-
-        // Set order details text
-        orderDetails.setText(orderID + customerID + products + order + totalPrice + "Thanks for shopping!");
-
-        // Add order details text area to the panel
-        panel.add(orderDetails);
-
-        // Add panel to the frame's content pane
-        frame.getContentPane().add(panel);
-
+        
+        orderDetails.setText(orderID + customerID + products + order + totalPrice + "Thanks for shopping!");        // Set order details text
+        
+        panel.add(orderDetails);        // Add order details text area to the panel
+        
+        frame.getContentPane().add(panel);          // Add panel to the frame
         ImageIcon image = new ImageIcon("C:\\Users\\HP\\Downloads\\icon-Copy-2.jpg");
         frame.setIconImage(image.getImage());
         frame.setSize(400, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);                 // Set frame properties
-
     }
 }
